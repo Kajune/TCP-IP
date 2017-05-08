73ebd0d5f0539082f395d093e5f8abe5cc696df0
Ichimaru <ryuzoji21@gmail.com>
2017-05-08 17:57:45 +0900

    TCP HTTP

--- TCP/Source.cpp
+++ TCP/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <WinSock2.h>
+#include <WS2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -18,8 +20,11 @@ int main() {
 
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
+	addr.sin_port = htons(80);
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	int yes = 1;
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&yes, sizeof(yes));
 	bind(sock, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr));
 
 	error = listen(sock, 5);
@@ -27,14 +32,30 @@ int main() {
 		std::cerr << WSAGetLastError() << std::endl;
 	}
 
-	sockaddr_in client;
-	int length = sizeof(client);
-	SOCKET sock1 = accept(sock, reinterpret_cast<sockaddr*>(&client), &length);
+	int count = 0;
+	while (true) {
+		sockaddr_in client;
+		int length = sizeof(client);
+		SOCKET sock1 = accept(sock, reinterpret_cast<sockaddr*>(&client), &length);
 
-	std::string message = "Hello, world!";
-	send(sock1, message.c_str(), message.length(), 0);
+		char inbuf[2048] = {};
+//		recv(sock1, inbuf, sizeof(inbuf), 0);
+//		std::cout << inbuf << std::endl;
 
-	closesocket(sock1);
+		std::string message = "Hello, world!";
+		message += std::to_string(count + 1) + "l–Ú";
+		send(sock1, message.c_str(), message.length(), 0);
+
+		char addr[16];
+		inet_ntop(AF_INET, &client.sin_addr, addr, sizeof(addr));
+		std::cout << "accepted connection from " << addr
+			<< ", port = " << ntohs(client.sin_port) << std::endl;
+
+		closesocket(sock1);
+		count++;
+	}
+
+	closesocket(sock);
 
 	WSACleanup();
 
--- TCP_Client/Source.cpp
+++ TCP_Client/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
@@ -17,21 +18,42 @@ int main() {
 		return -1;
 	}
 
+	std::string str;
+	std::cin >> str;
+
 	sockaddr_in server;
 	server.sin_family = AF_INET;
-	server.sin_port = htons(12345);
-	inet_pton(AF_INET, "10.200.12.86", &server.sin_addr);
-	
-	error = connect(sock, reinterpret_cast<const sockaddr*>(&server), sizeof(server));
-	if (error) {
-		std::cerr << WSAGetLastError() << std::endl;
+	server.sin_port = htons(80);
+	if (inet_pton(AF_INET, str.c_str(), &server.sin_addr) == 0) {
+		addrinfo hints{ 0 }, *res;
+		auto ret = getaddrinfo(str.c_str(), "http", &hints, &res);
+		while (res) {
+			server.sin_addr = ((sockaddr_in*)res->ai_addr)->sin_addr;
+			if (connect(sock, reinterpret_cast<const sockaddr*>(&server), sizeof(server)) == 0) {
+				break;
+			}
+			res = res->ai_next;
+		}
+	} else {
+		error = connect(sock, reinterpret_cast<const sockaddr*>(&server), sizeof(server));
+		if (error) {
+			std::cerr << WSAGetLastError() << std::endl;
+		}
 	}
 
-	char buf[32];
-	std::fill(std::begin(buf), std::end(buf), 0);
-	int n = recv(sock, buf, sizeof(buf), 0);
+	std::string message = "GET / HTTP/1.0\r\n\r\n";
+	send(sock, message.c_str(), message.size(), 0);
 
-	std::cout << n << ", " << buf << std::endl;
+	char buf[32] = {};
+	int n = 0;
+	do{
+		std::fill(std::begin(buf), std::end(buf), 0);
+		n = recv(sock, buf, sizeof(buf) - 1, 0);
+		buf[sizeof(buf) - 1] = '\0';
+		std::cout << buf;
+	}while (n > 0);
+
+	closesocket(sock);
 
 	WSACleanup();
 
